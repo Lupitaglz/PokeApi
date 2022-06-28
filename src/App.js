3b306928a15d44259a056f264e9febe35888094c
Lupitaglz <lupitaglzl84@gmail.com>
2022-06-27 22:46:46 -0500

    feat:Pokemon API

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+import { Usefetch } from "./components/Usefetch";
+import { Card } from "./components/card/Card";
+import './App.css';
+
+const App=()=>{
+  const[url,setUrl]=useState(' https://pokeapi.co/api/v2/pokemon/')
+  const estado =Usefetch(url)
+  const{cargando,data}=estado
+  cargando? console.log('cargando'):console.log(data.results)
+
+  return (
+      <div>
+          <h1>Lista de Pokemons</h1>
+
+          {
+              cargando
+              ?
+              <h1>Cargando...</h1>
+              :
+              <div className="caja">
+              <Card results = {data.results} />
+
+                  <div className="container ">
+                      <button onClick={()=>setUrl(data.previous)} className="btn">Anterior</button>
+                      <button onClick={()=>setUrl(data.next)} className="btn">Next</button>
+                  </div>
+
+              </div>
+          }
+
+      </div>
+  )
+}
+
+export default App;
